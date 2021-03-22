2cab126b0744da4a39478783d7e179155abdc112
MyLimonchik <kate.leznova@gmail.com>
2021-03-22 21:33:37 +0300

    init

---
+++ app/src/main/java/com/example/messenger/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.messenger.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
